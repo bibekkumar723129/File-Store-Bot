b803aace61cce5d56aef89ae711617db4d5f2a4e
Bibek Kumar Mahato <bibekkumar723129@gmail.com>
2025-08-26 09:10:25 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -39,17 +39,17 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     elif data == "buy_prem":
         await query.message.edit_text(
             text= (
-    f"👋 <b>Uɴɪғᴏʀᴍ Wᴇʟᴄᴏᴍᴇ, @{query.from_user.username}!</b>\n\n"
+    f"👋 <b>Wᴇʟᴄᴏᴍᴇ, @{query.from_user.username}!</b>\n\n"
     f"🌟 <b>Pʀɪᴍᴇ Mᴇᴍʙᴇʀsʜɪᴘ Pʟᴀɴs:</b>\n\n"
-    f"💫 <b>₹{PRICE1}</b> — <i>7 Dᴀʏs Aᴄᴄᴇss</i>\n"
-    f"💫 <b>₹{PRICE2}</b> — <i>1 Mᴏɴᴛʜ Aᴄᴄᴇss</i>\n"
-    f"💫 <b>₹{PRICE3}</b> — <i>3 Mᴏɴᴛʜs Aᴄᴄᴇss</i>\n"
-    f"💫 <b>₹{PRICE4}</b> — <i>6 Mᴏɴᴛʜs Aᴄᴄᴇss</i>\n"
-    f"💫 <b>₹{PRICE5}</b> — <i>1 Yᴇᴀʀ Aᴄᴄᴇss</i>\n\n"
+    f"💫 <b>{PRICE1}</b> — <i>7 Dᴀʏs Aᴄᴄᴇss</i>\n"
+    f"💫 <b>{PRICE2}</b> — <i>1 Mᴏɴᴛʜ Aᴄᴄᴇss</i>\n"
+    f"💫 <b>{PRICE3}</b> — <i>3 Mᴏɴᴛʜs Aᴄᴄᴇss</i>\n"
+    f"💫 <b>{PRICE4}</b> — <i>6 Mᴏɴᴛʜs Aᴄᴄᴇss</i>\n"
+    f"💫 <b>{PRICE5}</b> — <i>1 Yᴇᴀʀ Aᴄᴄᴇss</i>\n\n"
     f"💵 <b>Uᴘɪ ID:</b> <code>{UPI_ID}</code>\n\n"
     f"📸 <a href='{UPI_IMAGE_URL}'>Cʟɪᴄᴋ Hᴇʀᴇ ᴛᴏ Sᴄᴀɴ Qʀ Cᴏᴅᴇ</a>\n\n"
     f"🧾 <b>🔐 Aꜰᴛᴇʀ Pᴀʏᴍᴇɴᴛ:</b> Sᴇɴᴅ ᴀ sᴄʀᴇᴇɴsʜᴏᴛ.\n\n"
-    f"💬 <b>Nᴇᴇᴅ Hᴇʟᴘ?</b> <a href='https://t.me/DadyIsCalling'>Cᴏɴᴛᴀᴄᴛ @DadyIsCalling</a>"
+    f"💬 <b>Nᴇᴇᴅ Hᴇʟᴘ?</b> <a href='https://t.me/DadyIsCalling'>Cᴏɴᴛᴀᴄᴛ DadyIsCalling</a>"
 ),
             disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup(
